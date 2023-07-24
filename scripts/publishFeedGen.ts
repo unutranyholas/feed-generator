0cad69831ab92b6de3be309eeedf2428735a2052
Ihar Trafimovich <unutranyholas@gmail.com>
2023-07-24 13:34:55 +0200

    Update feed name

--- scripts/publishFeedGen.ts
+++ scripts/publishFeedGen.ts
@@ -21,7 +21,7 @@ const run = async () => {
 
   // A display name for your feed
   // Ex: What's Hot
-  const displayName = 'Belarusian Cluster'
+  const displayName = '⬜️🟥⬜ BEL Cluster'
 
   // (Optional) A description of your feed
   // Ex: Top trending content from the whole network
